6dcd6ac4d02a894957b5a896b2349b651f0d1fcc
HubReb <17859526+HubReb@users.noreply.github.com>
2025-12-07 21:29:37 +0100

    Completet Day Two.

--- DayTwo/Program.cs
+++ DayTwo/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Collections;
-using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 Console.WriteLine("Hello, World!");
@@ -22,7 +21,7 @@ while (streamReader.ReadLine() is { } line)
         var end = long.Parse(range[1]);
         for (var i = start; i <= end; ++i)
         {
-            if (!(ValidateId(i)))
+            if (!(ValidateIdPartTwo(i)))
             {
                 invalidIds.Add(i);
             }
@@ -43,4 +42,51 @@ bool ValidateId(long id)
     string firstHalf = idAsString.Substring(0, middle);
     string secondHalf = idAsString[^middle..];
     return !(firstHalf.Equals(secondHalf));
+}
+
+bool ValidateIdPartTwo(long id)
+{
+    var compoundedSequence = new ArrayList();
+    var sequence = id.ToString();
+    foreach (var t in sequence)
+    {
+        compoundedSequence.Add(t);
+        if (sequence.Length % compoundedSequence.Count != 0)
+        {
+            continue;
+        }
+        var length = compoundedSequence.Count;
+        if (IsSequenceRepeated(length)) return false;
+    }
+    return true;
+
+    bool IsSequenceRepeated(int length)
+    {
+        if (sequence.Length == 2)
+        {
+            return sequence[0].Equals(sequence[1]);
+        }
+
+        if (compoundedSequence.Count + length > sequence.Length)
+        {
+            return false;
+        }
+
+        var compoundedSequenceBuilder = new StringBuilder(compoundedSequence.Count);
+        foreach (var t in compoundedSequence) {
+            compoundedSequenceBuilder.Append(t);
+        }
+        var compareSequence = compoundedSequenceBuilder.ToString();
+        var skipLength = compoundedSequence.Count;
+        for (var j = length; (j + length) <= sequence.Length; j += skipLength)
+        {
+            var nextSequence = sequence[j..(j + length)];
+            if (!(nextSequence.Equals(compareSequence)))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
